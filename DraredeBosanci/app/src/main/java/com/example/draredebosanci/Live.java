e16cd5447f3eca5bd8d79859e941036131b4e72d
claudiuppdc <100693885+ImTheCloud@users.noreply.github.com>
2023-02-24 00:14:37 +0100

    ok

--- DraredeBosanci/app/src/main/java/com/example/draredebosanci/Live.java
+++ DraredeBosanci/app/src/main/java/com/example/draredebosanci/Live.java
@@ -1,7 +1,12 @@
 package com.example.draredebosanci;
 
+
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +21,8 @@ public class Live extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_live);
 
@@ -25,7 +32,7 @@ public class Live extends AppCompatActivity {
 
 
         Bundle extras = getIntent().getExtras();
-        if (extras != null) {
+
             String players = extras.getString("Players");
             String[] playerNames = players.split(",");
             List<String> playerList = new ArrayList<>(Arrays.asList(playerNames));
@@ -33,16 +40,61 @@ public class Live extends AppCompatActivity {
             int halfSize = playerList.size() / 2;
             List<String> firstHalf = playerList.subList(0, halfSize);
             List<String> secondHalf = playerList.subList(halfSize, playerList.size());
-            String playersString = TextUtils.join("\n", firstHalf);
-            TVPlayers.setText(playersString);
-            playersString = TextUtils.join("\n", secondHalf);
-            TVPlayers2.setText(playersString);
+        TVPlayers.setText("Team 1:\n");
+        String playersString = TextUtils.join("\n", firstHalf);
+        TVPlayers.append(playersString);
+
+        TVPlayers2.setText("Team 2:\n");
+        playersString = TextUtils.join("\n", secondHalf);
+        TVPlayers2.append(playersString);
 
 
-            String stopWatch = extras.getString("StopWatch");
-            TVStopWatch.setText(stopWatch+" '' ");
 
-        }
+
+        final CountDownTimer[] timer = {null};
+        final int[] totalTime = {45 * 60}; // Valeur par défaut de 45 minutes
+
+        Button addTimerButton = findViewById(R.id.add_timer_button);
+        addTimerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText timerEditText = findViewById(R.id.ID_Timer);
+                String timerString = timerEditText.getText().toString();
+                if (!TextUtils.isEmpty(timerString)) {
+                    try {
+                        int minutes = Integer.parseInt(timerString);
+                        totalTime[0] = minutes * 60;
+                    } catch (NumberFormatException e) {
+                        // La chaîne de caractères n'est pas un entier valide, on utilise la valeur par défaut
+                        totalTime[0] = 45*60;
+                    }
+                }
+
+                // Annuler le minuteur précédent s'il y en a un
+                if (timer[0] != null) {
+                    timer[0].cancel();
+                }
+
+                // Démarrer un nouveau minuteur
+                timer[0] = new CountDownTimer(totalTime[0] * 1000, 1000) {
+                    public void onTick(long millisUntilFinished) {
+                        int minutes = (int) millisUntilFinished / 60000;
+                        int seconds = (int) (millisUntilFinished % 60000) / 1000;
+                        String timeLeft = String.format("%02d:%02d", minutes, seconds);
+                        TVStopWatch.setText(timeLeft);
+                    }
+
+                    public void onFinish() {
+                        TVStopWatch.setText("00:00");
+                    }
+                }.start();
+            }
+        });
+
+
+
+
+
 
 
     }
--- DraredeBosanci/app/src/main/java/com/example/draredebosanci/NewGame.java
+++ DraredeBosanci/app/src/main/java/com/example/draredebosanci/NewGame.java
@@ -2,7 +2,7 @@ package com.example.draredebosanci;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -14,16 +14,18 @@ public class NewGame extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_game);
         ETPlayers = findViewById(R.id.ID_Player);
-        ETStopWatch = findViewById(R.id.PT_stopwatch);
+
     }
 
     public void goToLive(View v){
         Intent i = new Intent(NewGame.this, Live.class);
         i.putExtra("Players", ETPlayers.getText().toString());
-        i.putExtra("StopWatch", ETStopWatch.getText().toString());
+
 
         startActivity(i);
     }
