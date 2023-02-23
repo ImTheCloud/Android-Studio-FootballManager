d3e31c4a3a1d3c65a4776f02a53a3a88a6787fa2
claudiuppdc <100693885+ImTheCloud@users.noreply.github.com>
2023-02-23 21:41:22 +0100

    TESTOK

--- DraredeBosanci/app/src/main/java/com/example/draredebosanci/Live.java
+++ DraredeBosanci/app/src/main/java/com/example/draredebosanci/Live.java
@@ -1,12 +1,18 @@
 package com.example.draredebosanci;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Live extends AppCompatActivity {
-    private TextView TVPlayers;
+    private TextView TVPlayers, TVPlayers2, TVStopWatch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,11 +20,30 @@ public class Live extends AppCompatActivity {
         setContentView(R.layout.activity_live);
 
         TVPlayers = findViewById(R.id.TVPlayers);
+        TVPlayers2 = findViewById(R.id.TVPlayers2);
+        TVStopWatch = findViewById(R.id.TV_StopWatch);
+
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             String players = extras.getString("Players");
-            TVPlayers.setText(players);
+            String[] playerNames = players.split(",");
+            List<String> playerList = new ArrayList<>(Arrays.asList(playerNames));
+            Collections.shuffle(playerList);
+            int halfSize = playerList.size() / 2;
+            List<String> firstHalf = playerList.subList(0, halfSize);
+            List<String> secondHalf = playerList.subList(halfSize, playerList.size());
+            String playersString = TextUtils.join("\n", firstHalf);
+            TVPlayers.setText(playersString);
+            playersString = TextUtils.join("\n", secondHalf);
+            TVPlayers2.setText(playersString);
+
+
+            String stopWatch = extras.getString("StopWatch");
+            TVStopWatch.setText(stopWatch+" '' ");
+
         }
+
+
     }
 }
--- DraredeBosanci/app/src/main/java/com/example/draredebosanci/NewGame.java
+++ DraredeBosanci/app/src/main/java/com/example/draredebosanci/NewGame.java
@@ -1,6 +1,8 @@
 package com.example.draredebosanci;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -8,21 +10,21 @@ import android.widget.EditText;
 
 public class NewGame extends AppCompatActivity {
 
-    EditText ETTPlayers;
+    EditText ETPlayers, ETStopWatch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_game);
-        ETTPlayers = (EditText) findViewById(R.id.ID_Player);
-
+        ETPlayers = findViewById(R.id.ID_Player);
+        ETStopWatch = findViewById(R.id.PT_stopwatch);
     }
 
     public void goToLive(View v){
         Intent i = new Intent(NewGame.this, Live.class);
-        i.putExtra("Players", ETTPlayers.getText().toString());
+        i.putExtra("Players", ETPlayers.getText().toString());
+        i.putExtra("StopWatch", ETStopWatch.getText().toString());
+
         startActivity(i);
     }
-
-
 }
