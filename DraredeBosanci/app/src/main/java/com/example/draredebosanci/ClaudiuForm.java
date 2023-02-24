1e38e7a99a5c45a78818edc9665fcecd1bdee5bb
claudiuppdc <100693885+ImTheCloud@users.noreply.github.com>
2023-02-24 19:02:25 +0100

    Update ClaudiuForm.java

--- DraredeBosanci/app/src/main/java/com/example/draredebosanci/ClaudiuForm.java
+++ DraredeBosanci/app/src/main/java/com/example/draredebosanci/ClaudiuForm.java
@@ -13,26 +13,34 @@ public class ClaudiuForm extends AppCompatActivity {
 
     private EditText etWin;
     private EditText etTie;
+    private EditText etLose;
     private EditText etYellowCard;
-    private TextView et5Goal;
+    private EditText et5Goal;
     private TextView tvPointsWrite;
+    private TextView tvGameWrite;
+    private TextView tvWinRateWrite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_claudiu_form);
 
-        // Récupérer les trois EditText et les deux TextViews
+        // Récupérer les trois EditText et les trois TextViews
         etWin = findViewById(R.id.ETWin);
         etTie = findViewById(R.id.ETTie);
+        etLose = findViewById(R.id.ETLose);
         etYellowCard = findViewById(R.id.ETYellowCard);
         et5Goal = findViewById(R.id.ET5Goal);
         tvPointsWrite = findViewById(R.id.TVPointsWrite);
+        tvGameWrite = findViewById(R.id.TVGameWrite);
+        tvWinRateWrite = findViewById(R.id.TVWinRateWrite);
 
         // Ajouter les écouteurs d'événements de modification de texte pour chaque EditText
         etWin.addTextChangedListener(textWatcher);
         etTie.addTextChangedListener(textWatcher);
+        etLose.addTextChangedListener(textWatcher);
         etYellowCard.addTextChangedListener(textWatcher);
+        et5Goal.addTextChangedListener(textWatcher);
     }
 
     private final TextWatcher textWatcher = new TextWatcher() {
@@ -46,10 +54,15 @@ public class ClaudiuForm extends AppCompatActivity {
             // Mettre à jour le calcul lorsque le texte est modifié
             int valueWin = getValue(etWin.getText().toString());
             int valueTie = getValue(etTie.getText().toString());
+            int valueLose = getValue(etLose.getText().toString());
             int valueYellowCard = getValue(etYellowCard.getText().toString());
             int bonus5Goal = getValue(et5Goal.getText().toString());
+            int totalGames = valueWin + valueTie + valueLose;
+            double winRate = totalGames > 0 ? ((double) valueWin / totalGames) * 100 : 0.0;
             int points = ((valueWin * 3) + valueTie + bonus5Goal) - (valueYellowCard / 3);
             tvPointsWrite.setText(String.valueOf(points));
+            tvGameWrite.setText(String.valueOf(totalGames));
+            tvWinRateWrite.setText(String.format("%.0f%%", winRate));
         }
 
         @Override
