d66b2953f4a9858e1aa277cd2345400ebffe7177
claudiuppdc <100693885+ImTheCloud@users.noreply.github.com>
2023-02-24 13:54:58 +0100

    okk

--- DraredeBosanci/app/src/main/java/com/example/draredebosanci/Ranking.java
+++ DraredeBosanci/app/src/main/java/com/example/draredebosanci/Ranking.java
@@ -1,10 +1,19 @@
 package com.example.draredebosanci;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.TypedValue;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class Ranking extends AppCompatActivity {
 
@@ -13,6 +22,58 @@ public class Ranking extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranking);
     }
+
+    private Button newButton;
+
+    public void createNewButton(View view) {
+        // Demander à l'utilisateur de saisir un nom pour le bouton
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Name of the new player :");
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Définir le texte du bouton sur le nom saisi par l'utilisateur
+                String buttonText = input.getText().toString();
+                newButton = new Button(Ranking.this);
+                newButton.setText(buttonText);
+
+                // Définir les paramètres de layout avec des marges supérieure et inférieure
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.WRAP_CONTENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT
+                );
+                int marginInDp = (int) TypedValue.applyDimension(
+                        TypedValue.COMPLEX_UNIT_DIP, 10, getResources().getDisplayMetrics());
+                layoutParams.setMargins(0, marginInDp, 0, marginInDp);
+                newButton.setLayoutParams(layoutParams);
+
+                // Mettre la couleur de fond en bleu foncé et réduire l'espace entre les boutons
+                newButton.setBackgroundColor(Color.parseColor("#071227"));
+                LinearLayout parentLayout = findViewById(R.id.parent_layout);
+                parentLayout.setPadding(0, 0, 0, marginInDp);
+
+                // Ajouter le nouveau bouton au layout parent
+                parentLayout.addView(newButton);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
+    }
+
+
+
+
+
+
+
 
     public void goToClaudiuForm(View v){
         startActivity(new Intent(Ranking.this, ClaudiuForm.class));
