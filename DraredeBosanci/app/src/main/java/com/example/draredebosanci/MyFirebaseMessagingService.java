ef5bd3927fd72a9b18d02b1253e91e768029df50
claudiuppdc <100693885+ImTheCloud@users.noreply.github.com>
2023-03-03 13:26:47 +0100

    JK

---
+++ DraredeBosanci/app/src/main/java/com/example/draredebosanci/MyFirebaseMessagingService.java
@@ -0,0 +1,7 @@
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        // Traiter la notification reçue ici
+    }
+}
