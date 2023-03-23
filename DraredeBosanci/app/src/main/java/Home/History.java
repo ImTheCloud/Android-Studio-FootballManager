6314df5255b6731589bf7c164b34583fcf01e4e5
claudiuppdc <100693885+ImTheCloud@users.noreply.github.com>
2023-03-24 00:16:11 +0100

    7.2.1 (db history)

--- DraredeBosanci/app/src/main/java/Home/History.java
+++ DraredeBosanci/app/src/main/java/Home/History.java
@@ -1,18 +1,22 @@
 package Home;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.draredebosanci.R;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class History extends AppCompatActivity {
@@ -32,13 +36,47 @@ public class History extends AppCompatActivity {
         database.getReference("Game").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                // Convertir les données de la base de données Firebase en un objet Java
-                Map<String, Object> data = (Map<String, Object>) snapshot.getValue();
-                Gson gson = new Gson();
-                String json = gson.toJson(data);
+                // Extraire les données de la base de données Firebase pour les noms des joueurs et la localisation
+                Map<String, Object> gameData = (Map<String, Object>) snapshot.getValue();
+                Map<String, Object> teamsData = (Map<String, Object>) gameData.get("Teams");
+                Map<String, Object> mapData = (Map<String, Object>) gameData.get("Map");
+                Map<String, Object> goalsData = (Map<String, Object>) gameData.get("Goals");
+                Map<String, Object> timeData = (Map<String, Object>) gameData.get("Time");
+
+                // Extraire les données des noms des joueurs
+                String team1 = "";
+                String team2 = "";
+                for (Map.Entry<String, Object> entry : teamsData.entrySet()) {
+                    Map<String, Object> teamData = (Map<String, Object>) entry.getValue();
+                    ArrayList<String> players1 = (ArrayList<String>) teamData.get("team1");
+                    ArrayList<String> players2 = (ArrayList<String>) teamData.get("team2");
+                    team1 = String.join(", ", players1);
+                    team2 = String.join(", ", players2);
+                }
+
+                // Extraire les données de la localisation
+                String location = "";
+                for (Map.Entry<String, Object> entry : mapData.entrySet()) {
+                    Map<String, Object> mapPointData = (Map<String, Object>) entry.getValue();
+                    Map<String, Object> userLocationData = (Map<String, Object>) mapPointData.get("userLocation");
+                    String latitude = userLocationData.get("latitude").toString();
+                    String longitude = userLocationData.get("longitude").toString();
+                    location = "Latitude : " + latitude + " / Longitude : " + longitude;
+                }
+
+                // Extraire les données des scores
+                String scores = "";
+                for (Map.Entry<String, Object> entry : goalsData.entrySet()) {
+                    Map<String, Object> goalData = (Map<String, Object>) entry.getValue();
+                    String goalTeam1 = goalData.get("goalTeam1").toString();
+                    String goalTeam2 = goalData.get("goalTeam2").toString();
+                    scores += "Team 1: " + goalTeam1 + " / Team 2: " + goalTeam2 + "\n";
+                }
+
 
                 // Afficher les données dans le TextView
-                mTextView.setText(json);
+                String historyData = "Team 1 : " + team1 + "\n" + "Team 2 : " + team2 + "\n" + "Location : " + location+ "\n" + "scores : " + scores;
+                mTextView.setText(historyData);
             }
 
             @Override
@@ -48,3 +86,4 @@ public class History extends AppCompatActivity {
         });
     }
 }
+
