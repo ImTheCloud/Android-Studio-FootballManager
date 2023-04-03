6b24eb871fabec586dabf55f7bef4af042783296
claudiuppdc <100693885+ImTheCloud@users.noreply.github.com>
2023-04-04 00:50:20 +0200

    acces new game

--- DraredeBosanci/app/src/main/java/Home/Home.java
+++ DraredeBosanci/app/src/main/java/Home/Home.java
@@ -28,7 +28,7 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
     private boolean isSoundEnabled = true;
     private DrawerLayout drawerLayout;
     FloatingActionButton compo;
-    private Button newGameButton,oldGame,ranking;
+    private Button newGameButton,history,ranking;
     private FirebaseAuth mAuth;
 
     @Override
@@ -36,7 +36,7 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
 
-        oldGame = findViewById(R.id.BT_History);
+        history = findViewById(R.id.BT_History);
         ranking = findViewById(R.id.BT_Ranking);
         newGameButton = findViewById(R.id.BT_New_Game);
         drawerLayout = findViewById(R.id.drawer_layout);
@@ -58,98 +58,97 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
             navEmail.setText("Connected as :" +"\n"+ currentUser.getEmail());
         }
 
-            setSupportActionBar(toolbar);
-            navigationView.setNavigationItemSelectedListener(this);
-            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav);
-            drawerLayout.addDrawerListener(toggle);
-            toggle.syncState();
+        setSupportActionBar(toolbar);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
 
-            drawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
+        drawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+                newGameButton.setVisibility(View.INVISIBLE);
+                newGameButton.setEnabled(false);
+                history.setEnabled(false);
+                history.setVisibility(View.INVISIBLE);
+                ranking.setEnabled(false);
+                ranking.setVisibility(View.INVISIBLE);
+
+                layout_home.setEnabled(false);
+                layout_home.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onDrawerClosed(@NonNull View drawerView) {
+                history.setEnabled(true);
+                history.setVisibility(View.VISIBLE);
+                ranking.setEnabled(true);
+                ranking.setVisibility(View.VISIBLE);
+
+                newGameButton.setVisibility(View.VISIBLE);
+                newGameButton.setEnabled(true);
+                layout_home.setVisibility(View.VISIBLE);
+                layout_home.setEnabled(true);
+
+
+            }
+
+            @Override
+            public void onDrawerOpened(@NonNull View drawerView) {
+                // Nothing to do here
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+                // Nothing to do here
+            }
+        });
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+        String userEmail = null;
+        if (user != null) {
+            userEmail = user.getEmail();
+        }
+
+        if (userEmail != null) {
+            ranking.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
-                    newGameButton.setVisibility(View.INVISIBLE);
-                    newGameButton.setEnabled(false);
-                    oldGame.setEnabled(false);
-                    oldGame.setVisibility(View.INVISIBLE);
-                    ranking.setEnabled(false);
-                    ranking.setVisibility(View.INVISIBLE);
+                public void onClick(View view) {
+                    startActivity(new Intent(Home.this, Rank.class));
 
-                    layout_home.setEnabled(false);
-                    layout_home.setVisibility(View.INVISIBLE);
-                }
-
-                @Override
-                public void onDrawerClosed(@NonNull View drawerView) {
-                    oldGame.setEnabled(true);
-                    oldGame.setVisibility(View.VISIBLE);
-                    ranking.setEnabled(true);
-                    ranking.setVisibility(View.VISIBLE);
-
-                    newGameButton.setVisibility(View.VISIBLE);
-                    newGameButton.setEnabled(true);
-                    layout_home.setVisibility(View.VISIBLE);
-                    layout_home.setEnabled(true);
-
-
-                }
-
-                @Override
-                public void onDrawerOpened(@NonNull View drawerView) {
-                    // Nothing to do here
-                }
-
-                @Override
-                public void onDrawerStateChanged(int newState) {
-                    // Nothing to do here
                 }
             });
+            history.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startActivity(new Intent(Home.this, History.class));
+                }
+            });
+        } else {
+            ranking.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(getApplicationContext(), "Log in First", Toast.LENGTH_SHORT).show();
+                }
+            });
+            history.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(getApplicationContext(), "Log in First", Toast.LENGTH_SHORT).show();
 
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-            FirebaseAuth mAuth = FirebaseAuth.getInstance();
-            FirebaseUser user = mAuth.getCurrentUser();
-            String userEmail = null;
-            if (user != null) {
-                userEmail = user.getEmail();
-            }
-
-            if (userEmail != null) {
-                ranking.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        startActivity(new Intent(Home.this, Rank.class));
-
-                    }
-                });
-                newGameButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        startActivity(new Intent(Home.this, NewGame.class));
-
-                    }
-                });
-            } else {
-                ranking.setOnClickListener(new View.OnClickListener() {
-
-                    @Override
-                    public void onClick(View view) {
-                        Toast.makeText(getApplicationContext(), "Log in First", Toast.LENGTH_SHORT).show();
-                    }
-                });
-                newGameButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Toast.makeText(getApplicationContext(), "Log in First", Toast.LENGTH_SHORT).show();
-
-
-                    }
-                });
-            }
+                }
+            });
         }
+    }
 
 // on create end
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -163,59 +162,58 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
         if (user != null) {
             userEmail = user.getEmail();
         }
-            switch (item.getItemId()) {
-                case R.id.volume:
-                    if (isSoundEnabled) {
-                        // Désactiver le son
-                        isSoundEnabled = false;
-                        Intent intent = new Intent(Home.this, MyMusicService.class);
-                        startService(intent);
-                        item.setIcon(R.drawable.volume_off);
-                        item.setTitle("Volume off");
-                    } else {
-                        // Réactiver le son
-                        isSoundEnabled = true;
-                        Intent intent = new Intent(Home.this, MyMusicService.class);
-                        stopService(intent);
-                        item.setIcon(R.drawable.volume_up);
-                        item.setTitle("Volume up");
-                    }
-                    break;
-                case R.id.nav_about:
-                    startActivity(new Intent(Home.this, Contact.class));
-                    break;
+        switch (item.getItemId()) {
+            case R.id.volume:
+                if (isSoundEnabled) {
+                    // Désactiver le son
+                    isSoundEnabled = false;
+                    Intent intent = new Intent(Home.this, MyMusicService.class);
+                    startService(intent);
+                    item.setIcon(R.drawable.volume_off);
+                    item.setTitle("Volume off");
+                } else {
+                    // Réactiver le son
+                    isSoundEnabled = true;
+                    Intent intent = new Intent(Home.this, MyMusicService.class);
+                    stopService(intent);
+                    item.setIcon(R.drawable.volume_up);
+                    item.setTitle("Volume up");
+                }
+                break;
+            case R.id.nav_about:
+                startActivity(new Intent(Home.this, Contact.class));
+                break;
 
-                case R.id.nav_logout:
+            case R.id.nav_logout:
 
-                    if (userEmail != null) {
-                        mAuth.signOut();
-                        startActivity(new Intent(Home.this, Login.class));
-                    }else{
-                        Toast.makeText(getApplicationContext(), "Already disconnected", Toast.LENGTH_SHORT).show();
-                    }
-                    break;
-                case R.id.nav_login:
-                    if (userEmail != null) {
-                        Toast.makeText(getApplicationContext(), "Already connected", Toast.LENGTH_SHORT).show();
-                    }else{
-                        startActivity(new Intent(Home.this, Login.class));
-                    }
-                    break;
-                case R.id.nav_compo:
-                    startActivity(new Intent(Home.this, Compo.class));
+                if (userEmail != null) {
+                    mAuth.signOut();
+                    startActivity(new Intent(Home.this, Login.class));
+                }else{
+                    Toast.makeText(getApplicationContext(), "Already disconnected", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.nav_login:
+                if (userEmail != null) {
+                    Toast.makeText(getApplicationContext(), "Already connected", Toast.LENGTH_SHORT).show();
+                }else{
+                    startActivity(new Intent(Home.this, Login.class));
+                }
+                break;
+            case R.id.nav_compo:
+                startActivity(new Intent(Home.this, Compo.class));
 
-                    break;
+                break;
 
-            }
-            newGameButton.setEnabled(false);
-            return true;
         }
+        newGameButton.setEnabled(false);
+        return true;
+    }
 
-
-
-    public void goToHistory(View v){
-        startActivity(new Intent(Home.this, History.class));
+    public void goToNewGame(View v){
+        startActivity(new Intent(Home.this, NewGame.class));
     }
 
 
 }
+
