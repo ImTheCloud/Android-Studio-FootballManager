d03b0719a4a248dbebb78b35a8a01133ba407130
claudiuppdc <100693885+ImTheCloud@users.noreply.github.com>
2023-04-11 01:01:35 +0200

    UX ranking + login register ux regler clavier

---
+++ DraredeBosanci/app/src/main/java/Firebase/RankSave.java
@@ -0,0 +1,136 @@
+package Firebase;
+
+import android.widget.EditText;
+
+public class RankSave {
+
+
+    EditText name1, name2, name3, name4, name5, name6, name7, name8, name9, name10, name11, name12;
+    EditText point1, point2, point3, point4, point5, point6, point7, point8, point9, point10, point11, point12;
+
+
+    public RankSave(EditText name1, EditText name2, EditText name3, EditText name4, EditText name5, EditText name6, EditText name7, EditText name8, EditText name9, EditText name10, EditText name11, EditText name12, EditText point1, EditText point2, EditText point3, EditText point4, EditText point5, EditText point6, EditText point7, EditText point8, EditText point9, EditText point10, EditText point11, EditText point12) {
+        this.name1 = name1;
+        this.name2 = name2;
+        this.name3 = name3;
+        this.name4 = name4;
+        this.name5 = name5;
+        this.name6 = name6;
+        this.name7 = name7;
+        this.name8 = name8;
+        this.name9 = name9;
+        this.name10 = name10;
+        this.name11 = name11;
+        this.name12 = name12;
+        this.point1 = point1;
+        this.point2 = point2;
+        this.point3 = point3;
+        this.point4 = point4;
+        this.point5 = point5;
+        this.point6 = point6;
+        this.point7 = point7;
+        this.point8 = point8;
+        this.point9 = point9;
+        this.point10 = point10;
+        this.point11 = point11;
+        this.point12 = point12;
+    }
+
+    public EditText getName1() {
+        return name1;
+    }
+
+    public EditText getName2() {
+        return name2;
+    }
+
+    public EditText getName3() {
+        return name3;
+    }
+
+    public EditText getName4() {
+        return name4;
+    }
+
+    public EditText getName5() {
+        return name5;
+    }
+
+    public EditText getName6() {
+        return name6;
+    }
+
+    public EditText getName7() {
+        return name7;
+    }
+
+    public EditText getName8() {
+        return name8;
+    }
+
+    public EditText getName9() {
+        return name9;
+    }
+
+    public EditText getName10() {
+        return name10;
+    }
+
+    public EditText getName11() {
+        return name11;
+    }
+
+    public EditText getName12() {
+        return name12;
+    }
+
+    public EditText getPoint1() {
+        return point1;
+    }
+
+    public EditText getPoint2() {
+        return point2;
+    }
+
+    public EditText getPoint3() {
+        return point3;
+    }
+
+    public EditText getPoint4() {
+        return point4;
+    }
+
+    public EditText getPoint5() {
+        return point5;
+    }
+
+    public EditText getPoint6() {
+        return point6;
+    }
+
+    public EditText getPoint7() {
+        return point7;
+    }
+
+    public EditText getPoint8() {
+        return point8;
+    }
+
+    public EditText getPoint9() {
+        return point9;
+    }
+
+    public EditText getPoint10() {
+        return point10;
+    }
+
+    public EditText getPoint11() {
+        return point11;
+    }
+
+    public EditText getPoint12() {
+        return point12;
+    }
+
+
+}
--- DraredeBosanci/app/src/main/java/Ranking/Rank.java
+++ DraredeBosanci/app/src/main/java/Ranking/Rank.java
@@ -3,14 +3,76 @@ package Ranking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.draredebosanci.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import Firebase.Form;
+import Firebase.RankSave;
 
 public class Rank extends AppCompatActivity {
+
+    EditText name1, name2, name3, name4, name5, name6, name7, name8, name9, name10, name11, name12;
+    EditText point1, point2, point3, point4, point5, point6, point7, point8, point9, point10, point11, point12;
+    Button bt_Save;
+    DatabaseReference UserRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rank);
+
+        point1 = findViewById(R.id.point1);
+        point2 = findViewById(R.id.point2);
+        point3 = findViewById(R.id.point3);
+        point4 = findViewById(R.id.point4);
+        point5 = findViewById(R.id.point5);
+        point6 = findViewById(R.id.point6);
+        point7 = findViewById(R.id.point7);
+        point8 = findViewById(R.id.point8);
+        point9 = findViewById(R.id.point9);
+        point10 = findViewById(R.id.point10);
+        point11 = findViewById(R.id.point11);
+        point12 = findViewById(R.id.point12);
+
+        name1 = findViewById(R.id.name1);
+        name2 = findViewById(R.id.name2);
+        name3 = findViewById(R.id.name3);
+        name4 = findViewById(R.id.name4);
+        name5 = findViewById(R.id.name5);
+        name6 = findViewById(R.id.name6);
+        name7 = findViewById(R.id.name7);
+        name8 = findViewById(R.id.name8);
+        name9 = findViewById(R.id.name9);
+        name10 = findViewById(R.id.name10);
+        name11 = findViewById(R.id.name11);
+        name12 = findViewById(R.id.name12);
+
+        bt_Save = findViewById(R.id.bt_Save);
+        bt_Save.setVisibility(View.INVISIBLE);
+
+        if (TextUtils.equals("claudiuppdc7@yahoo.com", FirebaseAuth.getInstance().getCurrentUser().getEmail())) {
+            bt_Save.setVisibility(View.VISIBLE);
+
+        }
+        bt_Save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    RankSave data = new RankSave(name1, name2, name3, name4, name5, name6, name7, name8, name9, name10, name11, name12,point1, point2, point3, point4, point5, point6, point7, point8, point9, point10, point11, point12);
+                    String uniqueId = "-rank"; // utiliser le même identifiant unique
+                    UserRef = FirebaseDatabase.getInstance("https://drare-de-bosanci-default-rtdb.europe-west1.firebasedatabase.app/").getReference().child("Rank/rank");
+                    UserRef.child(uniqueId).setValue(data); 
+                    Toast.makeText(Rank.this, "Ranking save", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 }
