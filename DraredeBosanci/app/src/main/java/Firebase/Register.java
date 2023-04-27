7eb43662681dfa2f2220d2d9815c50994744d033
claudiuppdc <100693885+ImTheCloud@users.noreply.github.com>
2023-04-27 13:13:53 +0200

    champs emogie ok

--- DraredeBosanci/app/src/main/java/Firebase/Register.java
+++ DraredeBosanci/app/src/main/java/Firebase/Register.java
@@ -1,4 +1,5 @@
 package Firebase;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
@@ -7,7 +8,6 @@ import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 import com.example.draredebosanci.R;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -34,12 +34,21 @@ public class Register extends AppCompatActivity {
             public void onClick(View view) {
                 String user = signupEmail.getText().toString().trim();
                 String pass = signupPassword.getText().toString().trim();
-                if (user.isEmpty()){
+                String regex = "^[a-zA-Z0-9]+$";
+                if (user.isEmpty()) {
                     signupEmail.setError("Email cannot be empty");
+                } else if (!user.matches(regex)) {
+                    signupEmail.setError("Email can only contain letters and numbers");
+                } else if (!user.matches("^[\\p{L}A-Za-z0-9]+$")) {
+                    signupEmail.setError("Email can only contain letters, numbers and symbols");
                 }
-                if (pass.isEmpty()){
+                if (pass.isEmpty()) {
                     signupPassword.setError("Password cannot be empty");
-                } else{
+                } else if (!pass.matches(regex)) {
+                    signupPassword.setError("Password can only contain letters and numbers");
+                } else if (!pass.matches("^[\\p{L}A-Za-z0-9]+$")) {
+                    signupPassword.setError("Password can only contain letters, numbers and symbols");
+                } else {
                     auth.createUserWithEmailAndPassword(user, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
@@ -47,14 +56,13 @@ public class Register extends AppCompatActivity {
                                 auth.getCurrentUser().sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
                                     @Override
                                     public void onComplete(@NonNull Task<Void> task) {
-                                        if(task.isSuccessful() ){
-                                            Toast.makeText(Register.this,"User registered, verify your email",Toast.LENGTH_SHORT).show();
+                                        if (task.isSuccessful()) {
+                                            Toast.makeText(Register.this, "User registered, verify your email", Toast.LENGTH_SHORT).show();
                                             signupEmail.setText("");
                                             signupPassword.setText("");
                                             startActivity(new Intent(Register.this, Login.class));
                                             overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
-                                        }else
-                                        {
+                                        } else {
                                             Toast.makeText(Register.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                                         }
                                     }
